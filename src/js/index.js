51c8e6a3fcfb50e5ade77e06fa5bf9b82807a0ff
Marta Probierz <marta.anna.probierz@gmail.com>
2022-08-12 14:09:14 +0200

    add task1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
-// Here will be dragons
+import { tns } from 'tiny-slider';
+
+tns({
+  container: '.slider',
+  items: 1,
+  autoplay: true,
+  speed: 600,
+  autoplayButtonOutput: false,
+  loop: true,
+  gutter: 0,
+  edgePadding: 0,
+  controlsText: [
+    `<span class='fa fa-arrow-left'></span>`,
+    `<span class='fa fa-arrow-right'></span>`,
+  ],
+});
