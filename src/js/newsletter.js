c1a2e7916f4e18b12ed294300a33a54892d4c71c
Marta Probierz <marta.anna.probierz@gmail.com>
2022-08-13 00:54:38 +0200

    add task2

---
+++ src/js/newsletter.js
@@ -0,0 +1,48 @@
+const modal = document.getElementById('modal-overlay');
+const closeModalButton = document.getElementById('close-modal');
+const inputEmail = document.querySelector('.email');
+const email = document.querySelector('input[type=email]');
+const textError = document.querySelector('.textError');
+const checkboxInput = document.getElementById('terms');
+const checkError = document.querySelector('.checkError');
+const radioInputs = document.getElementsByTagName('input');
+const radioError = document.querySelector('.radioError');
+const submitButton = document.querySelector('.button');
+
+const showModalWindow = () => {
+  modal.style.display = 'flex';
+};
+setTimeout(showModalWindow, 6000);
+
+const hideModalWindow = () => {
+  modal.style.display = 'none';
+};
+closeModalButton.addEventListener('click', hideModalWindow);
+
+const validateEmail = (str) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(str).toLowerCase());
+};
+
+submitButton.addEventListener('click', () => {
+  if (validateEmail(email.value)) {
+    textError.classList.remove('error');
+    inputEmail.classList.remove('errorInt');
+  } else {
+    textError.classList.add('error');
+    inputEmail.classList.add('errorInt');
+  }
+  if (!checkboxInput.checked) {
+    checkError.classList.add('error');
+  } else {
+    checkError.classList.remove('error');
+  }
+  if (true) {
+    for (let i = 0; i < radioInputs.length; i++) {
+      return radioInputs[i].type === 'radio' && radioInputs[i].checked
+        ? radioError.classList.remove('error')
+        : radioError.classList.add('error');
+    }
+  }
+});
