b7638aa0698e504d7ffecf40319cee8b02ca3f48
Marta Probierz <marta.anna.probierz@gmail.com>
2022-08-14 20:36:35 +0200

    add task3

--- src/js/newsletter.js
+++ src/js/newsletter.js
@@ -9,12 +9,14 @@ const radioInputs = document.getElementsByTagName('input');
 const radioError = document.querySelector('.radioError');
 const spinner = document.querySelector('.spinner');
 const form = document.querySelector('form');
+const addedInfo = document.querySelector('.addedInfo');
+const already = document.querySelector('.already');
 const submitButton = document.querySelector('.button');
 
 const showModalWindow = () => {
   modal.style.display = 'flex';
 };
-setTimeout(showModalWindow, 1000);
+setTimeout(showModalWindow, 6000);
 
 const hideModalWindow = () => {
   modal.style.display = 'none';
@@ -47,13 +49,43 @@ submitButton.addEventListener('click', () => {
   }
 });
 
-function handleSubmit(event) {
-  event.preventDefault();
+const SUBSCRIBERS_URL = 'http://localhost:8888/subscribers';
+const req = new XMLHttpRequest();
+const check = [];
+
+req.open('GET', SUBSCRIBERS_URL, true);
+req.onload = () => {
+  const result = JSON.parse(req.responseText);
+  return Array.isArray(result) ? result.map((e) => check.push(e.email)) : [];
+};
+req.send();
+
+const handleSubmit = (e) => {
+  e.preventDefault();
   spinner.classList.add('added');
   radioError.classList.remove('error');
-  const data = new FormData(event.target);
-  const value = Object.fromEntries(data.entries());
-  console.log({ value });
-}
+  const data = {
+    email: email.value,
+    gender: document.querySelector('input[name="gender"]:checked').value,
+    'pp-check': checkboxInput.value,
+  };
+
+  req.open('POST', SUBSCRIBERS_URL, true);
+  req.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  req.onreadystatechange = () => {
+    if (check.includes(email.value)) {
+      spinner.classList.remove('added');
+      already.classList.add('added');
+    } else if (req.readyState === 4 && req.status === 201) {
+      already.classList.remove('added');
+      spinner.classList.remove('added');
+      addedInfo.classList.add('added');
+      setTimeout(hideModalWindow, 6000);
+    }
+  };
+  !check.includes(email.value)
+    ? req.send(JSON.stringify(data))
+    : req.send(null);
+};
 
 form.addEventListener('submit', handleSubmit);
