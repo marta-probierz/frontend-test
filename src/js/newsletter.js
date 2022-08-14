d8fc110f2a9eac80d6c420a571cdadef067305ac
Marta Probierz <marta.anna.probierz@gmail.com>
2022-08-14 14:43:05 +0200

    add spinner

--- src/js/newsletter.js
+++ src/js/newsletter.js
@@ -7,12 +7,14 @@ const checkboxInput = document.getElementById('terms');
 const checkError = document.querySelector('.checkError');
 const radioInputs = document.getElementsByTagName('input');
 const radioError = document.querySelector('.radioError');
+const spinner = document.querySelector('.spinner');
+const form = document.querySelector('form');
 const submitButton = document.querySelector('.button');
 
 const showModalWindow = () => {
   modal.style.display = 'flex';
 };
-setTimeout(showModalWindow, 6000);
+setTimeout(showModalWindow, 1000);
 
 const hideModalWindow = () => {
   modal.style.display = 'none';
@@ -38,11 +40,20 @@ submitButton.addEventListener('click', () => {
   } else {
     checkError.classList.remove('error');
   }
-  if (true) {
-    for (let i = 0; i < radioInputs.length; i++) {
-      return radioInputs[i].type === 'radio' && radioInputs[i].checked
-        ? radioError.classList.remove('error')
-        : radioError.classList.add('error');
-    }
+  for (let i = 0; i < radioInputs.length; i++) {
+    return radioInputs[i].type === 'radio' && radioInputs[i].checked
+      ? radioError.classList.remove('error')
+      : radioError.classList.add('error');
   }
 });
+
+function handleSubmit(event) {
+  event.preventDefault();
+  spinner.classList.add('added');
+  radioError.classList.remove('error');
+  const data = new FormData(event.target);
+  const value = Object.fromEntries(data.entries());
+  console.log({ value });
+}
+
+form.addEventListener('submit', handleSubmit);
